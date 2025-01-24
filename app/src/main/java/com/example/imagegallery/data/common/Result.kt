027b489ad40dd3c6c9b51d92e2e478f7e622f5e3
WeiWW <wei.juin.lin@gmail.com>
2025-01-24 16:18:31 +0900

    Add new result type for 401 Unauthorized

--- app/src/main/java/com/example/imagegallery/data/common/Result.kt
+++ app/src/main/java/com/example/imagegallery/data/common/Result.kt
@@ -3,15 +3,20 @@ package com.example.imagegallery.data.common
 sealed class Result<out R> {
     data class Success<out T>(val data:T) : Result<T>()
     data class Error(val errorMsg:String) : Result<Nothing>()
+    object Unauthorized : Result<Nothing>()
 
     override fun toString(): String = when (this) {
         is Success<*> -> "Success[data=$data]"
         is Error -> "Error[exception=$errorMsg]"
+        is Unauthorized -> "Error[exception=401Unauthorized]"
     }
 }
 
 val Result<*>.isSuccess
     get() = this is Result.Success
+
+val Result<*>.is401Error
+    get() = this is Result.Unauthorized
 
 fun <T> Result<T>.getOrNull(): T? = (this as? Result.Success)?.data
 
